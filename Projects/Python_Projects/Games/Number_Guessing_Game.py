6fb2dde052114405d768d0b5fe02cffde3ac967e
Andrea Venti Fuentes <andrea.venti12@gmail.com>
2023-03-31 19:05:47 -0400

    updated the number guessing game

--- Projects/Python_Projects/Games/Number_Guessing_Game.py
+++ Projects/Python_Projects/Games/Number_Guessing_Game.py
@@ -1,52 +1,50 @@
 import random
 
-user_decision = input(
-    "Do you want to play a number guessing game? (y/n): ").lower()
+print("Welcome to the number guessing game!")
+print("You have to guess a number between 1 and 30.")
+print("You will have 5 attempts to guess the number.")
+print("If you guess the number, you win!")
+print("If you don't, you lose!")
+print("Good luck!\n")
+
+user_decision = input("Do you want to play? (y/n): ").lower()
 if user_decision != "y" and user_decision != "yes":
     print("\nGoodbye!")
     quit()
 
 while True:
-    while True:
-        n = random.randint(1, 30)
-        counter = 0
-        number = int(input("\nGuess a number between 1 and 30: "))
-        if (number == n):
-            print("\nYou guessed correctly!!! Go you!")
-            counter += 1
+    print("\nGuess a number between 1 and 30: ")
 
-        while (number < 1) or (number > 30):
-            number = int(
-                input("\nThat is an invalid number. Please re-enter a number between 1 and 30: "))
-            if (number == n):
-                print("\nYou guessed correctly!!! Go you!")
-
-        while number != n:
-            if (number <= 5) and (n > 5):
-                number = int(input("\nToo low! Try again: "))
+    def guess_number(counter):
+        while counter < 5:
+            try:
+                number = int(input("> "))
                 counter += 1
-            elif (number >= 25) and (n < 25):
-                number = int(input("\nWay too high! Try again: "))
-                counter += 1
-            elif ((n + 5 >= number) and (n - 5 <= number)):
-                number = int(input("\nYou are so close! Try again: "))
-                counter += 1
-            else:
-                number = int(input("\nWrong! Try again: "))
-                counter += 1
-            if (number == n):
-                print("\nYou guessed correctly!!! Go you!")
-            while (number < 1) or (number > 30):
-                number = int(
-                    input("\nThat is an invalid number. Please re-enter a number between 1 and 30: "))
+                if (number < 1) or (number > 30):
+                    continue
+                elif number == random_number:
+                    print(
+                        f"\nCongratulations! You guessed the number in {counter} attempts!\n")
+                    return True
+                elif (random_number - 5 <= number <= random_number + 5):
+                    print("\nYou are so close! Try again.")
+                elif number > random_number:
+                    print("\nThe number is lower than that. Try again.")
+                elif number < random_number:
+                    print("\nThe number is higher than that. Try again.")
 
-        print(f"You took {counter} attempts to guess the number!")
+            except ValueError:
+                print("\nPlease enter a valid number.")
 
-        if number == n:
-            break
+        print(
+            f"\nSorry, you ran out of attempts. The number was {random_number}\n")
+        return False
 
-    play_again = input("\nDo you want to play again? (y/n): ").lower()
+    random_number = random.randint(1, 30)
     counter = 0
+    result = guess_number(counter)
+
+    play_again = input("Do you want to play again? (y/n): ").lower()
     if play_again != "y" and play_again != "yes":
-        print("\nYour loss....")
+        print("\nThanks for playing!")
         break
