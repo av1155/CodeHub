d8cda08ea78c7188b07ea881862d8c28608836cd
Andrea Venti Fuentes <andrea.venti12@gmail.com>
2023-03-31 11:27:39 -0400

    updated contents of simulation with functions

--- Projects/Python_Projects/Numerical_Computation/Population_Growth_Calculator/Population_Growth_Simulation.py
+++ Projects/Python_Projects/Numerical_Computation/Population_Growth_Calculator/Population_Growth_Simulation.py
@@ -1,40 +1,58 @@
-# The input below records the starting number of organisms.
-starting_num_organisms = int(input("Enter the starting number of organisms: "))
-# The while loop below validates the input and requests the user to input a new value if the number is negative or zero.
-while starting_num_organisms <= 0:
-    starting_num_organisms = int(input(
-        "Negative values or zero for the starting number of organisms is not allowed. Please re-enter the starting number of organisms: "))
-print()
+# Main function
+def main():
 
-# The input below records the average daily population increase.
-daily_population_increase = float(input(
-    "Enter the average daily population increase (number will be translated to percent value, for example, 30 means 30%): "))
-# The while loop below validates the input and requests the user to input a new value if the numbers are either negative or larger than 100.
-while (daily_population_increase < 0) or (daily_population_increase > 100):
+    print("\nAndrea A. Venti Fuentes")
+
+    first_input = get_starting_num_organisms()
+    print()
+    second_input = get_daily_population_increase()
+    print()
+    third_input = get_days_to_multiply()
+    print()
+
+    print("\nDay\t\t Approximate Population")
+    for day in range(1, (third_input + 1)):
+        print(f"{day} \t\t {round(first_input, 5)}")
+        first_input *= second_input
+
+
+def get_starting_num_organisms():
+
+    # The input below records the starting number of organisms.
+    starting_num_organisms = int(
+        input("Enter the starting number of organisms: "))
+    # The while loop below validates the input and requests the user to input a new value if the number is negative or zero.
+    while starting_num_organisms <= 0:
+        starting_num_organisms = int(input(
+            "Negative values or zero for the starting number of organisms is not allowed. Please re-enter the starting number of organisms: "))
+    return starting_num_organisms
+
+
+def get_daily_population_increase():
+
+    # The input below records the average daily population increase.
     daily_population_increase = float(input(
-        "Negative values or values larger than 100 are not allowed. Please re-enter the average daily population increase: "))
+        "Enter the average daily population increase (number will be translated to percent value, for example, 30 means 30%): "))
+    # The while loop below validates the input and requests the user to input a new value if the numbers are either negative or larger than 100.
+    while (daily_population_increase < 0) or (daily_population_increase > 100):
+        daily_population_increase = float(input(
+            "Negative values or values larger than 100 are not allowed. Please re-enter the average daily population increase: "))
 
-# The equation below converts the daily population increase value into a positive multiplication percentage so it can be used later in the program.
-# For example, 30 + 100 = 130, dividing 130 by 100 = 1.3
-# This number becomes the final percent multiplicator. For example: 1.3 * 2 = 2.6. That is a 30% increase to the original value.
-daily_population_increase = ((daily_population_increase + 100) / 100)
+    daily_population_increase = ((daily_population_increase + 100) / 100)
 
-print()
+    return daily_population_increase
 
-# The input below records the number of days that the organisms will be left to multiply for.
-days_to_multiply = int(
-    input("Enter the number of days the organism will multiply for: "))
-# The while loop below validates the input and requests the user to input a new value if the numbers are either negative, zero, or larger than 30.
-while (days_to_multiply <= 0) or (days_to_multiply > 30):
-    days_to_multiply = int(input(
-        "Negative values, zero, or values larger than 30 are not allowed. Please re-enter the number of days the organism will be left to multiply for: "))
-print()
 
-# The print statement below prints the table header where the days and the approximate population will be displayed.
-print("Day\t\t Approximate Population")
+def get_days_to_multiply():
 
-# The for loop below creates a range from 1 to the number of days that the organism will be left to multiply for. A +1 is added so that the range goes from 1 to the exact number, and not one less. For example, if the user inputs 3, the range will go from 1 to 3, not 1 to 2. The loop will run as many times as the number of days the organism will be left to multiply for. The loop will also print the day and the approximate population for each day. The population is calculated by multiplying the starting number of organisms by the daily population increase and continuously updating the value of the starting number of organisms. The population will be displayed with 5 decimal places for ease of reading and uniformity.
+    # The input below records the number of days that the organisms will be left to multiply for.
+    days_to_multiply = int(
+        input("Enter the number of days the organism will multiply for: "))
+    # The while loop below validates the input and requests the user to input a new value if the numbers are either negative, zero, or larger than 30.
+    while (days_to_multiply <= 0) or (days_to_multiply > 30):
+        days_to_multiply = int(input(
+            "Negative values, zero, or values larger than 30 are not allowed. Please re-enter the number of days the organism will be left to multiply for: "))
+    return days_to_multiply
 
-for days in range(1, (days_to_multiply + 1)):
-    print(f"{days} \t\t {round(starting_num_organisms, 5)}")
-    starting_num_organisms *= daily_population_increase
+
+main()  # Very last statement in code
