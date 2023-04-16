f8a599e005f06779cecd1d771ec0a29043b7f53d
Andrea Venti Fuentes <andrea.venti12@gmail.com>
2023-04-15 22:53:24 -0400

    finished hackathon project

--- Projects/Python_Projects/PlanetPath/Streamlit_PlanetPath.py
+++ Projects/Python_Projects/PlanetPath/Streamlit_PlanetPath.py
@@ -1,6 +1,7 @@
 import sys
 import streamlit as st
 from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderServiceError
 from geopy.distance import geodesic
 
 
@@ -8,128 +9,142 @@ def get_distance(place1, place2):
     # Create a geocoder object
     geolocator = Nominatim(user_agent="my-app")
 
-    # Geocode the place names to get their coordinates
+    try:
+        # Geocode the place names to get their coordinates
+        location1 = geolocator.geocode(place1)
+        location2 = geolocator.geocode(place2)
 
-    location1 = geolocator.geocode(place1)
-    location2 = geolocator.geocode(place2)
+        # Check if geocoding was successful
+        if not location1 or not location2:
+            st.write("Invalid place name. Please enter a valid place name.")
+            return None
 
-    # Calculate the distance between the two coordinates using geodesic distance
+        # Calculate the distance between the two coordinates using geodesic distance
+        distance = geodesic((location1.latitude, location1.longitude),
+                            (location2.latitude, location2.longitude)).miles
 
-    # if location1 is an existing attribute do this
+        return distance
 
-    distance = geodesic((location1.latitude, location1.longitude),
-                        (location2.latitude, location2.longitude)).miles
-
-    return distance
+    except (AttributeError, GeocoderServiceError) as e:
+        st.error(f"Error: {e}")
+        return None
 
 
 def walking(distance):
     emissions = distance * 404
+    if emissions > 1000:
+        emissions = round(emissions / 453.592, 2)
+        unit = "pounds"
+    else:
+        unit = "grams"
     points = round(distance * 10)
     st.write(
-        f"\nYou just saved {round(emissions, 2)} grams of CO2 emissions by walking {round(distance, 2)} miles with PlanetPath. Keep up the good work for a greener planet!")
+        f"\nYou just saved {round(emissions, 2)} {unit} of CO2 emissions by walking {round(distance, 2)} miles with PlanetPath. Keep up the good work for a greener planet!")
     st.write(
         f"\nYou've earned {points} points for walking {round(distance, 2)} miles with PlanetPath!")
-
-    exit()
+    return points
 
 
 def biking(distance):
     emissions = distance * 404
+    if emissions > 1000:
+        emissions = round(emissions / 453.592, 2)
+        unit = "pounds"
+    else:
+        unit = "grams"
     points = round(distance * 10)
     st.write(
-        f"\nYou just saved {round(emissions, 2)} grams of CO2 emissions by biking {round(distance, 2)} miles with PlanetPath. Keep up the good work for a greener planet!")
+        f"\nYou just saved {round(emissions, 2)} {unit} of CO2 emissions by biking {round(distance, 2)} miles with PlanetPath. Keep up the good work for a greener planet!")
     st.write(
         f"\nYou've earned {points} points for biking {round(distance, 2)} miles with PlanetPath!")
-    exit()
+    return points
 
 
 def bus(distance):
     emissions = distance * 209
+    if emissions > 1000:
+        emissions = round(emissions / 453.592, 2)
+        unit = "pounds"
+    else:
+        unit = "grams"
     points = round(distance * 5)
-    st.write(f"\nYou just saved {round(emissions, 2)} grams of CO2 emissions by using the bus for {round(distance, 2)} miles with PlanetPath. Keep up the good work for a greener planet!")
+    st.write(f"\nYou just saved {round(emissions, 2)} {unit} of CO2 emissions by using the bus for {round(distance, 2)} miles with PlanetPath. Keep up the good work for a greener planet!")
     st.write(
         f"\nYou've earned {points} points for using the bus for {round(distance, 2)} miles with PlanetPath!")
-    exit()
+    return points
 
 
 def ride_share(distance):
-    number_of_people = st.text_input(
-        ("How many people are in the car with you?\n> "))
-    number_of_people = int(number_of_people)
-
-    emissions = distance * 404
-    total_emissions = emissions / number_of_people
+    emissions = (distance * 404) - 404
+    if emissions > 1000:
+        emissions = round((emissions / 453.592)/2, 2)
+        unit = "pounds"
+    else:
+        unit = "grams"
     points = round(distance * 3)
     st.write(
-        f"\nEach passenger saved {round(total_emissions, 2)} grams of CO2 emissions by using ride share for {round(distance, 2)} miles with PlanetPath. Keep up the good work for a greener planet!")
+        f"\nEach passenger saved {round(emissions, 2)} {unit} of CO2 emissions by using ride share for {round(distance, 2)} miles with PlanetPath. Keep up the good work for a greener planet!")
     st.write(
         f"\nYou've earned {points} points for using ride share for {round(distance, 2)} miles with PlanetPath!")
-    exit()
+    return points
+
+
+def driving(distance):
+    emissions = distance * 404
+    if emissions > 1000:
+        emissions = round((emissions / 453.592)/2, 2)
+        unit = "pounds"
+    else:
+        unit = "grams"
+    st.write(
+        f"\nBy driving alone, you would produce {round(emissions, 2)} {unit} of CO2 emissions for {round(distance, 2)} miles of travel. Consider using a more sustainable transportation method or carpooling to reduce your carbon footprint.")
 
 
 def main():
-    try:
-        sys.tracebacklimit = 0  # Turn off traceback error message
-        st.set_page_config(
-            page_title="PlanetPath",
-            page_icon=":earth_africa:",
-            layout="wide",
-        )
-        logo = 'https://green-transfer.fr/wp-content/uploads/2019/08/Green_Leaf_Earth_PNG_Clipart-2978.png'
+    distance = 0
+    st.set_page_config(
+        page_title="PlanetPath",
+        page_icon=":earth_africa:",
+        layout="wide",
+    )
+    logo = 'https://green-transfer.fr/wp-content/uploads/2019/08/Green_Leaf_Earth_PNG_Clipart-2978.png'
 
-        st.image(logo, width=365)
-        st.title("PlanetPath")
+    st.image(logo, width=300)
+    st.title("PlanetPath")
 
-        while True:
-            try:
-                # Prompt user to enter place names
-                place1 = st.text_input("\nWhat is your starting point? ")
-                if place1 == 'exit':
-                    sys.exit()
+    form = st.form(key='input-form')
+    with form:
+        # Prompt user to enter place names
+        place1 = st.text_input("What is your starting point?")
+        place2 = st.text_input("What is your destination?")
 
-                place2 = st.text_input("\nWhat is your destination? ")
-                if place2 == 'exit':
-                    sys.exit()
+        # Prompt user to choose transportation option
+        choice = st.selectbox("Choose a transportation option:", [
+                              "Walking", "Biking", "Bus", "Ride share", "Driving"])
 
-                # Get the distance between the two places
-                distance = get_distance(place1, place2)
+        submit_button = st.form_submit_button(label='Calculate')
 
-                # Display CO2 emissions and points for each transportation option
-                st.write(
-                    f"\nCO2 emissions and points for {round(distance, 2)} miles:\n")
-                st.write(
-                    f"1) Walking: {round(distance * 404, 2)} grams of CO2 emissions, {round(distance * 10)} points")
-                st.write(
-                    f"2) Biking: {round(distance * 404, 2)} grams of CO2 emissions, {round(distance * 10)} points")
-                st.write(
-                    f"3) Bus: {round(distance * 209, 2)} grams of CO2 emissions, {round(distance * 5)} points")
-                st.write(
-                    f"4) Ride share: {round(distance * 404, 2)} grams of CO2 emissions potentially saved by all passengers, {round(distance * 3)} points")
+    # Get the distance between the two places after the user submits the form
+    if submit_button:
+        # Get the distance between the two places
+        distance = get_distance(place1, place2)
 
-                # Prompt user to choose transportation option
-                choice = st.text_input(
-                    "\nPlease choose a transportation option (1-4): ")
-                if choice == 'exit':
-                    sys.exit()
-                choice = int(choice)
+        # Display CO2 emissions and points for the selected transportation option
+        if choice == "Walking":
+            walking(distance)
+        elif choice == "Biking":
+            biking(distance)
+        elif choice == "Bus":
+            bus(distance)
+        elif choice == "Ride share":
+            ride_share(distance)
+        elif choice == "Driving":
+            driving(distance)
+        else:
+            st.write("Invalid choice. Please choose a transportation option.")
 
-                # Call appropriate function based on transportation option
-                if choice == 1:
-                    walking(distance)
-                elif choice == 2:
-                    biking(distance)
-                elif choice == 3:
-                    bus(distance)
-                elif choice == 4:
-                    ride_share(distance)
-                else:
-                    st.write("Invalid choice. Please choose a number from 1-4.")
-
-            except ValueError:
-                pass
-    except:
-        pass
+    if st.button("Home Page"):
+        st.experimental_rerun()
 
 
 if (__name__ == "__main__"):
