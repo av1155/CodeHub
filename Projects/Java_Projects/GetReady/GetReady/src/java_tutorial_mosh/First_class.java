70975a61173d663a48435ad69acb2b128444c7a5
Andrea Venti Fuentes <andrea.venti12@gmail.com>
2023-08-21 16:40:43 -0400

    created a package to learn Java from tutorial

---
+++ Projects/Java_Projects/GetReady/GetReady/src/java_tutorial_mosh/First_class.java
@@ -0,0 +1,9 @@
+package java_tutorial_mosh;
+
+public class First_class {
+    // `psvm`
+    public static void main(String[] args) {
+        // `sout`
+        System.out.println("The following is my learning experience by watching the Java tutorial from Mosh");
+    }
+}
