42011b575012bf84d0df846514a758b2494559e1
Andrea Venti Fuentes <andrea.venti12@gmail.com>
2023-08-21 16:53:27 -0400

    Added private static function and called it within the main function

--- Projects/Java_Projects/GetReady/GetReady/src/java_tutorial_mosh/First_class.java
+++ Projects/Java_Projects/GetReady/GetReady/src/java_tutorial_mosh/First_class.java
@@ -3,7 +3,15 @@ package java_tutorial_mosh;
 public class First_class {
     // `psvm`
     public static void main(String[] args) {
-        // `sout`
+        // `sout` creates `System.out.println();`
         System.out.println("The following is my learning experience by watching the Java tutorial from Mosh");
+
+        // `PrintHelloWorld();` calls the private function `private static void PrintHelloWorld()`... which contains within the curly brackets `System.out.println("HelloWorld);`
+        PrintHelloWorld();
     }
+
+    private static void PrintHelloWorld() {
+        System.out.println("HelloWorld");
+    }
+
 }
